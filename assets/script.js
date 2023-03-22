dcdfd9741cd75fe170cf29f86fbeaca243c62f0d
KeltonSterett <keltonsterett24@gmail.com>
2023-03-21 23:22:18 -0600

    added ids to css, listed HTMl ids to be used in JS

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+// listing HTML ids to be used in the script
+var firstPage = document.getElementById("firstPage");
+var quizPage = document.getElementById("quiz");
+var resultsPage = document.getElementById("quiz-results");
+var startQuizbutton = document.getElementById("start-btn");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+var timer = document.getElementById("timer");
+var finalScore = document.getElementById("final-score");
+var gameover = document.getElementById("gameover");
+var questions = document.getElementById("questions");
+var highscoresContainer = document.getElementById("highscores-container");
+var highscoresDiv = document.getElementById("highscorePage");
+var highscoresInputname = document.getElementById("initials");
+var HighscoresName = document.getElementById("highscores-intials");
+var highscoresDisplayScore = document.getElementById("highscores-score");
+var endGameBtn = document.getElementById("endGameBtns");
+var submitScoreBtns = document.getElementById("submit");
+
+    
