9a29474c3bb40787cc24052ff6b4034a37d7ee84
KeltonSterett <keltonsterett24@gmail.com>
2023-03-22 22:04:48 -0600

    final commit, added all functions and event listners for questions, high scores, added event listeners

--- assets/script.js
+++ assets/script.js
@@ -13,8 +13,8 @@ var gameover = document.getElementById("gameover");
 var questions = document.getElementById("questions");
 var highscoresContainer = document.getElementById("highscores-container");
 var highscoresDiv = document.getElementById("highscorePage");
-var highscoresInputname = document.getElementById("initials");
-var HighscoresName = document.getElementById("highscores-intials");
+var highscoresInput = document.getElementById("initials");
+var highscoresName = document.getElementById("highscores-initials");
 var highscoresDisplayScore = document.getElementById("highscores-score");
 var endGameBtn = document.getElementById("endGameBtns");
 var submitScoreBtns = document.getElementById("submit");
@@ -24,10 +24,10 @@ var quizQuestions = [{
     // question property
 question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
 // choices for the question
-choiceA: "<script href='xxx.js'>",
-choiceB: "<script name='xxx.js'>",
-choiceC: "<script src='xxx.js'>",
-choiceD: "<script file='xxx.js'>",
+choiceA: "script href='xxx.js",
+choiceB: "script name='xxx.js'",
+choiceC: "script src='xxx.js'",
+choiceD: "script file='xxx.js'",
 // correct answer property
 correctAnswer: "c"},
 {
@@ -53,10 +53,10 @@ choiceD: "function myFunction()",
 correctAnswer: "c"},
 {
     question: "Iniside which HTML element do we put the JavaScript?",
-    choiceA: "<script>",
-    choiceB: "<js>",
-    choiceC: "<javascript>",
-    choiceD: "<scripting>",
+    choiceA: "script",
+    choiceB: "js",
+    choiceC: "javascript",
+    choiceD: "scripting",
     correctAnswer: "a"},
 
 ];
@@ -81,7 +81,7 @@ function generateQuizQuestion() {
     buttonC.innerHTML = currentQuestion.choiceC;
     buttonD.innerHTML = currentQuestion.choiceD;
 };
-//  this 
+//  this function checks to see if the answer is correct or not and adds or subtracts time accordingly
 function startQuiz() {
     gameover.style.display = "none";
     firstPage.style.display = "none";
@@ -98,3 +98,107 @@ function startQuiz() {
     }, 1000);
     quizPage.style.display = "block";
 }
+// This function is the end page screen that displays your score after either completeing the quiz or upon timer run out
+function showScore(){
+    quizPage.style.display = "none"
+    gameover.style.display = "flex";
+    clearInterval(timerInterval);
+    highscoresInput.value = "";
+    finalScore.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
+}
+
+// On click of the submit button, we run the function highscore that saves and stringifies the array of high scores already saved in local stoage
+// as well as pushing the new user name and score into the array we are saving in local storage. Then it runs the function to show high scores.
+submitScoreBtns.addEventListener("click", function highscore(){
+    
+    
+    if(highscoresInput.value === "") {
+        alert("Initials cannot be blank");
+        return false;
+    }else{
+        var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+        var currentUser = highscoresInput.value.trim();
+        var currentHighscore = {
+            name : currentUser,
+            score : score
+        };
+    
+        gameover.style.display = "none";
+        highscoresContainer.style.display = "flex";
+        highscoresDiv.style.display = "block";
+        endGameBtns.style.display = "flex";
+        
+        savedHighscores.push(currentHighscore);
+        localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+
+        generateHighscores();
+
+    }
+    
+});
+
+// This function clears the list for the high scores and generates a new high score list from local storage
+function generateHighscores(){
+    highscoresName.innerHTML = "";
+    highscoresDisplayScore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+    for (i=0; i<highscores.length; i++){
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        highscoresName.appendChild(newNameSpan);
+        highscoresDisplayScore.appendChild(newScoreSpan);
+    }
+}
+
+// This function displays the high scores page while hiding all of the other pages from 
+function showHighscore(){
+    firstPage.style.display = "none"
+    gameover.style.display = "none";
+    highscoresContainer.style.display = "flex";
+    highscoresDiv.style.display = "block";
+    endGameBtns.style.display = "flex";
+
+    generateHighscores();
+}
+
+// This function clears the local storage of the high scores as well as clearing the text from the high score board
+function clearScore(){
+    window.localStorage.clear();
+    highscoresName.textContent = "";
+    highscoresDisplayScore.textContent = "";
+}
+
+// This function sets all the variables back to their original values and shows the home page to enable replay of the quiz
+function replayQuiz(){
+    highscoresContainer.style.display = "none";
+    gameover.style.display = "none";
+    firstPage.style.display = "flex";
+    timeLeft = 76;
+    score = 0;
+    currentQuestionIndex = 0;
+}
+
+// This function checks the response to each answer 
+function checkAnswer(answer){
+    correct = quizQuestions[currentQuestionIndex].correctAnswer;
+
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex){
+        score++;
+        alert("That Is Correct!");
+        currentQuestionIndex++;
+        generateQuizQuestion();
+        //display in the results div that the answer is correct.
+    }else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex){
+        alert("That Is Incorrect.")
+        currentQuestionIndex++;
+        generateQuizQuestion();
+        //display in the results div that the answer is wrong.
+    }else{
+        showScore();
+    }
+}
+
+// This button starts the quiz!
+startQuizbutton.addEventListener("click",startQuiz);
